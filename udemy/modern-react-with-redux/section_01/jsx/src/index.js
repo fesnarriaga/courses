92a6bdde9cb02bca24ed31539d4a96222f844fcb
Felipe Esnarriaga <f_esnarriaga@yahoo.com.br>
2020-12-08 20:33:08 -0300

    End of module

--- udemy/modern-react-with-redux/section_01/jsx/src/index.js
+++ udemy/modern-react-with-redux/section_01/jsx/src/index.js
@@ -1,17 +1,22 @@
+// Import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Create a react component
+function App() {
+  return (
+    <div>Hello world</div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App1 = function () {
+  return <div>Hello world</div>
+}
+
+const App2 = () => {
+  return <div>Hello world</div>
+}
+
+// Render the component on screen
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.querySelector('#root'));
