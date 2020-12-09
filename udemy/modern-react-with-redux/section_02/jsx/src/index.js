a6f9ee76da2024df7d7c514b2efdea381d61186f
Felipe Esnarriaga <f_esnarriaga@yahoo.com.br>
2020-12-08 21:02:24 -0300

    Lesson 15 amend

--- udemy/modern-react-with-redux/section_02/jsx/src/index.js
+++ udemy/modern-react-with-redux/section_02/jsx/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>
-    <label class="label" for="name">Enter name:</label>
-    <input id="name" type="text" />
-    <button style="background-color: blue; color: white;">Submit</button>
-  </div>
-    ;
+  return (
+    <div>
+      <label class="label" for="name">Enter name:</label>
+      <input id="name" type="text" />
+      <button style="background-color: blue; color: white;">Submit</button>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
