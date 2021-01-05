10e0287e48bbd4d275486d867b5691cd077ea5e9
Felipe Esnarriaga <felipe.esnarriaga@apinteractive.com.br>
2021-01-05 08:42:05 -0300

    Partial view and view component

---
+++ desenvolvedor-io/dominando-o-aspnet-mvc-core/01 - Views Examples/ViewsExamples/ViewsExamples/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ViewsExamples.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ desenvolvedor-io/dominando-o-aspnet-mvc-core/01 - Views Examples/ViewsExamples/ViewsExamples/ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ViewsExamples.ViewComponents
+{
+    [ViewComponent(Name = "MeuCarrinho")]
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public int ItensNoCarrinho { get; set; }
+
+        public CarrinhoViewComponent()
+        {
+            ItensNoCarrinho = 5;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(ItensNoCarrinho);
+        }
+    }
+}
