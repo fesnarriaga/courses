74def23a4a00450878b425480eacdce8241c8431
Felipe Esnarriaga <f_esnarriaga@yahoo.com.br>
2021-03-01 21:12:01 -0300

    End of module

--- udemy/dart-and-flutter_the-complete-developers-guide/section_08/pics/lib/main.dart
+++ udemy/dart-and-flutter_the-complete-developers-guide/section_08/pics/lib/main.dart
@@ -3,19 +3,5 @@ import 'package:flutter/material.dart';
 import './src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Let\'s See Images!'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('Hi there!');
-        },
-      ),
-    ),
-  );
-
-  runApp(app);
+  runApp(App());
 }
--- udemy/dart-and-flutter_the-complete-developers-guide/section_08/pics/lib/src/app.dart
+++ udemy/dart-and-flutter_the-complete-developers-guide/section_08/pics/lib/src/app.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Let\'s See Images'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('Hi there!');
-        },
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Let\'s See Images!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              counter++;
+            });
+          },
+        ),
+        body: Text('$counter'),
       ),
     );
   }
