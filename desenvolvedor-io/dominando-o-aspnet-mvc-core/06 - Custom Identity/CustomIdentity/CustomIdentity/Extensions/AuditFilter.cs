0f846b341541ea8e5802a23473264034ab0a82e4
Felipe Esnarriaga <felipe.esnarriaga@apinteractive.com.br>
2021-03-16 08:21:05 -0300

    Final configurations

---
+++ desenvolvedor-io/dominando-o-aspnet-mvc-core/06 - Custom Identity/CustomIdentity/CustomIdentity/Extensions/AuditFilter.cs
@@ -0,0 +1,28 @@
+﻿using KissLog;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CustomIdentity.Extensions
+{
+    public class AuditFilter : IActionFilter
+    {
+        private readonly ILogger _logger;
+
+        public AuditFilter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context) { }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.HttpContext.User.Identity != null && context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                var message = $"{context.HttpContext.User.Identity.Name} => {context.HttpContext.Request.GetDisplayUrl()}";
+
+                _logger.Info(message);
+            }
+        }
+    }
+}
