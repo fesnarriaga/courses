0a275da678c9f17da4431d0bfb8fbbcaaf676889
Felipe Esnarriaga <felipe.esnarriaga@apinteractive.com.br>
2021-04-13 21:00:00 -0300

    complete first app

--- desenvolvedor-io/spa-com-angular/01_PrimeiraApp/FirstApp/src/app/app.module.ts
+++ desenvolvedor-io/spa-com-angular/01_PrimeiraApp/FirstApp/src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+// Modules
+import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { ComponentNameComponent } from './folder/component-name/component-name.component';
+
+// Services
+import { SomeServiceService } from './some-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentNameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    SomeServiceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ desenvolvedor-io/spa-com-angular/01_PrimeiraApp/FirstApp/src/app/some-feature/some-feature.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    // Components
+  ],
+  imports: [
+    CommonModule
+    // Modules
+  ],
+  providers: [
+    // Services
+  ]
+})
+export class SomeFeatureModule { }
