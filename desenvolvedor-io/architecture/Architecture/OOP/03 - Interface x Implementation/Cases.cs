2021b919fbb3e07ca42051db82bc75a704ce39ed
Felipe Esnarriaga <felipe.esnarriaga@apinteractive.com.br>
2021-05-03 15:33:55 -0300

    oop final

---
+++ desenvolvedor-io/architecture/Architecture/OOP/03 - Interface x Implementation/Cases.cs
@@ -0,0 +1,63 @@
+﻿namespace OOP
+{
+    public interface IRepository
+    {
+        void Add();
+    }
+
+    public class Repository : IRepository
+    {
+        public void Add()
+        {
+            // Do something
+        }
+    }
+
+    public class RepositoryFake : IRepository
+    {
+        public void Add()
+        {
+            // Do something
+        }
+    }
+
+    public class UsesImplementation
+    {
+        public void Process()
+        {
+            var repository = new Repository();
+
+            repository.Add();
+        }
+    }
+
+    public class UsesAbstraction
+    {
+        private readonly IRepository _repository;
+
+        public UsesAbstraction(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Process()
+        {
+            _repository.Add();
+        }
+    }
+
+    public class TestInterfaceImplementation
+    {
+        public TestInterfaceImplementation()
+        {
+            var repositoryImplementation = new UsesImplementation();
+            repositoryImplementation.Process();
+
+            var repositoryAbstraction = new UsesAbstraction(new Repository());
+            repositoryAbstraction.Process();
+
+            var repositoryFakeAbstraction = new UsesAbstraction(new RepositoryFake());
+            repositoryFakeAbstraction.Process();
+        }
+    }
+}
---
+++ desenvolvedor-io/architecture/Architecture/OOP/04 - Inheritance x Composition/Cases.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace OOP
+{
+    public class CustomerInheritance : Person
+    {
+        public string Document { get; set; }
+    }
+
+    public class CustomerComposition
+    {
+        public Person Person { get; set; }
+        public string Document { get; set; }
+    }
+
+    public class TestsInheritanceComposition
+    {
+        public TestsInheritanceComposition()
+        {
+            var customerInheritance = new CustomerInheritance
+            {
+                Name = "John Doe",
+                BirthDate = DateTime.Now,
+                Document = "12345678901"
+            };
+
+            var customerComposition = new CustomerComposition
+            {
+                Person = new Person
+                {
+                    Name = "John Doe",
+                    BirthDate = DateTime.Now
+                },
+                Document = "12345678901"
+            };
+
+            var nameInheritance = customerInheritance.Name;
+            var nameComposition = customerComposition.Person.Name;
+        }
+    }
+
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+
+        void Remove(T entity);
+    }
+
+    public interface IPersonRepository
+    {
+        void Add(Person person);
+    }
+
+    public class Repository<T> : IRepository<T>
+    {
+        public void Add(T entity)
+        {
+            // Do something
+        }
+
+        public void Remove(T entity)
+        {
+            // Do something
+        }
+    }
+
+    public class RepositoryInheritance : Repository<Person>, IPersonRepository { }
+
+    public class RepositoryComposition : IPersonRepository
+    {
+        private readonly IRepository<Person> _repository;
+
+        public RepositoryComposition(IRepository<Person> repository)
+        {
+            _repository = repository;
+        }
+
+        public void Add(Person person)
+        {
+            _repository.Add(person);
+        }
+    }
+
+    public class TestsInheritanceCompositionRepositories
+    {
+        public TestsInheritanceCompositionRepositories()
+        {
+            var inheritanceRepository = new RepositoryInheritance();
+            inheritanceRepository.Add(new Person());
+            inheritanceRepository.Remove(new Person());
+
+            var compositionRepository = new RepositoryComposition(new Repository<Person>());
+            compositionRepository.Add(new Person());
+        }
+    }
+}
